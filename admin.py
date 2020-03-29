4ac74874d14480f4e753b1e10d6d4af6768caf1c
Cunego Daniele <Daniele.Cunego@cattolicaITS.it>
2020-03-29 17:03:14 +0200

    La mia prima App, primo commit

---
+++ admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import RegistrazioneTurno
+
+# Register your models here.
+
+admin.site.register(RegistrazioneTurno)
---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Appprova01Config(AppConfig):
+    name = 'appprova01'
